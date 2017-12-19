b021760eff5fb1db1f4948f65db3fcb779a10390
anuar2k <anuar2k@outlook.com>
2017-12-19 18:36:08 +0100

    Fixing update

--- src/main/java/org/terasology/additionalRails/components/RailSwitchComponent.java
+++
@@ -1,9 +0,0 @@
-package org.terasology.additionalRails.components;
-
-import org.terasology.entitySystem.Component;
-
-public class RailSwitchComponent implements Component {
-    public boolean isOn;
-    public boolean hasSignal;
-    public int mode;
-}
